b71b4d714bb5ef50bde040bac683092bfc4da81a
Gabriel Tadeu Matiolla <gabrielmatiolla@gmail.com>
2025-05-07 20:27:00 -0300

    alterações

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ class Memento:
         self._estado = estado
 
     def get_estado_salvo(self):
-        return self._estado
+        return self._estado # Retorna o estado armazenado
 
 
 class Originador:
     """Cria e restaura seu estado através de Mementos"""
     def __init__(self):
-        self._estado = ""
+        self._estado = "" # Estado inicial vazio
 
     def set_estado(self, estado):
         print(f"Originador: Definindo estado para {estado}")
-        self._estado = estado
+        self._estado = estado # Altera o estado atual
 
     def salvar_para_memento(self):
         print("Originador: Salvando para Memento.")
-        return Memento(self._estado)
+        return Memento(self._estado) # Cria um Memento com o estado atual
 
     def restaurar_de_memento(self, memento):
-        self._estado = memento.get_estado_salvo()
+        self._estado = memento.get_estado_salvo() # Restaura estado do Memento
         print(f"Originador: Estado após restaurar do Memento: {self._estado}")
 
 
 class Cuidador:
     """Responsável por guardar os Mementos"""
     def __init__(self, originador):
-        self._originador = originador
-        self._mementos = []
+        self._originador = originador # Referência ao Originador
+        self._mementos = [] # Lista para armazenar os estados
 
     def backup(self):
         print("\nCuidador: Salvando estado do Originador...")
@@ -39,7 +39,7 @@ class Cuidador:
         if not self._mementos:
             return
 
-        memento = self._mementos.pop()
+        memento = self._mementos.pop() # Remove o último Memento da lista
         print(f"Cuidador: Restaurando estado para: {memento.get_estado_salvo()}")
         self._originador.restaurar_de_memento(memento)
 
